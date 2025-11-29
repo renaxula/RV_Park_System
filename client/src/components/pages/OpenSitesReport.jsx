ed34276dbf16b3c8c4d50682e8ec0280670f2126
Greg_Raisor <raisor.greg@gmail.com>
2025-11-29 16:30:26 -0700

    openSitesReport-added filtering capability

--- client/src/components/pages/OpenSitesReport.jsx
+++ client/src/components/pages/OpenSitesReport.jsx
@@ -1,15 +1,50 @@
+import { useState } from "react";
 import CurrentAvailability from "../reports/CurrentAvailability";
 import { Card } from "../ui/Card";
 
 export function OpenSitesReport() {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [error, setError] = useState();
+
+  const createFilterHandler = (event) => {
+    event.preventDefault();
+    const start = document.getElementById("start").value;
+    const end = document.getElementById("end").value;
+
+    if (new Date(end) < new Date(start)) {
+      setError("End must be after start");
+      return;
+    }
+    if (start != end) {
+      setStartDate(start);
+      setEndDate(end);
+    } else {
+      setStartDate(start);
+    }
+    setError("");
+
+    //console.log("start: " + startDate);
+    //console.log("end: " + endDate);
+  };
+
   return (
     <Card>
       <h2>Open Sites Report</h2>
       <p>
         Shows which sites are available, nights available, and rate per night
-        for each date.
+        for each date. By default gets today's availability, use date selectors
+        to get availability for different day/date range.
       </p>
-      <CurrentAvailability />
+      <label htmlFor="start">Start: </label>
+      <input type="date" id="start" name="start" />
+      <label htmlFor="end"> End: </label>
+      <input type="date" id="end" name="end" />
+      <div>
+        <button onClick={createFilterHandler}>Search</button>
+        <p>{error}</p>
+      </div>
+      <CurrentAvailability startDate={startDate} endDate={endDate} />
     </Card>
   );
 }
